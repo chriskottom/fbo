f541cff07acc3074846da77cb17b24c0033bfa26
Chris Kottom <chris@chriskottom.com>
2013-10-16 20:49:01 +0200

    make the world safe for non-UTF characters

--- lib/fbo/file.rb
+++ lib/fbo/file.rb
@@ -5,7 +5,7 @@ module FBO
     extend Forwardable
 
     attr_reader     :file
-    def_delegators  :@file, :open, :readline, :read, :path, :to_path
+    def_delegators  :@file, :open, :readline, :read, :path, :to_path, :eof?
 
     class << self
       def filename_for_date(date)
@@ -16,6 +16,23 @@ module FBO
 
     def initialize(filename)
       @file = ::File.new(filename)
+    end
+
+    def contents
+      if @contents.nil?
+        @contents = cleanup_data(@file.read)
+      end
+      @contents
+    end
+
+    private
+
+
+    def cleanup_data(data)
+      data.encode('UTF-16le', :invalid => :replace, :replace => '')
+          .encode('UTF-8')
+          .gsub(/\r\n/, "\n")
+          .gsub(/^M/, "")
     end
   end
 end
--- spec/fbo/file_spec.rb
+++ spec/fbo/file_spec.rb
@@ -9,10 +9,18 @@ describe FBO::File do
         proc { subject.filename_for_date(nil) }.must_raise ArgumentError
       end
 
-      it 'should give the right name for a given date' do
+      it 'returns the right file name for a given date' do
         date = Date.parse('2013-07-08')
         subject.filename_for_date(date).must_equal 'FBOFeed20130708'
       end
+    end
+  end
+
+  context 'non-existent file' do
+    subject          { FBO::File.new('foobar.bat') }
+
+    it 'raises an exception' do
+      proc { subject.readline }.must_raise Errno::ENOENT
     end
   end
 
@@ -20,16 +28,10 @@ describe FBO::File do
     let(:filename)   { File.join(File.dirname(__FILE__), '..', 'fixtures', 'FBOFeed20130331') }
     subject          { FBO::File.new(filename) }
 
-    it 'should open the file and read its contents' do
-      subject.readline.wont_be_nil
-    end
-  end
-
-  context 'non-existent file' do
-    subject          { FBO::File.new('foobar.bat') }
-
-    it 'should raise an exception' do
-      proc { subject.readline }.must_raise Errno::ENOENT
+    it 'enables access to file contents' do 
+      subject.contents.must_be_instance_of String
+      subject.contents.wont_be_empty
+      subject.contents.must_match /^<PRESOL>\n<DATE>0331/
     end
   end
 end
